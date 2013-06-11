a0ec25955cf5ffea26547fd32df27fc781997ad8
Jorge Janaite Neto <jjn@Jorge-Janaite-Netos-iMac-2.local>
2013-06-11 18:46:20 -0300

    Initial Commit

---
+++ coredataExemplo1/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  coredataExemplo1
+//
+//  Created by Jorge Janaite Neto on 6/11/13.
+//  Copyright (c) 2013 Jorge Janaite Neto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "JJNAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([JJNAppDelegate class]));
+    }
+}
